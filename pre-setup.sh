e5e3ba46c7e1f2d05600702eac711b88d853ac38
onezach <87786580+onezach@users.noreply.github.com>
2024-02-13 08:49:36 -0600

    cleanup

--- pre-setup.sh
+++ pre-setup.sh
@@ -2,4 +2,6 @@ sudo apt update
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash ./Miniconda3-latest-Linux-x86_64.sh
 
+rm -f ./Miniconda3-latest-Linux-x86_64.sh
+
 exec bash
