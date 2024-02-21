3e43ea0e09a60f70b8ff22727a749e15db5b32f5
alicia-lyu <wlyu27@wisc.edu>
2024-02-21 11:01:46 -0600

    Edit logging

--- run.sh
+++ run.sh
@@ -3,29 +3,31 @@
 WORKERS_PORT=('60001' '60002' '60003')
 HOST_NAME='cs744@c220g2-011110.wisc.cloudlab.us'
 PART_IDS=("2a" "2b" "3")
+LOG_DIR="$HOME/CS744-assignment2/logs"  # Define a base directory for logs
 
 part() {
     local part_id=$1
     local rank=$2
     local worker_port=$3
-    ssh -p "$worker_port" "$HOST_NAME" "cd ~/CS744-assignment2/main/part$part_id && ./run-p$part_id.sh $rank" > "./logs/rank$rank" &
+    # Redirect output to a log file within the log directory
+    ssh -p "$worker_port" "$HOST_NAME" "cd ~/CS744-assignment2/main/part$part_id && ./run-p$part_id.sh $rank" > "${LOG_DIR}/part${part_id}/rank${rank}.log" 2>&1 &
     echo "Started part$part_id with rank $rank on $HOST_NAME:$worker_port"
 }
 
 part1() {
     echo "Starting part1"
-    cd ~/CS744-assignment2/main/part1 && ./run-p1.sh 0 > ./part1.log
+    cd ~/CS744-assignment2/main/part1 && ./run-p1.sh 0 > "${LOG_DIR}/part1/log" 2>&1
 }
 
 main() {
     part1
     # part 2a, 2b, 3
     for part_id in "${PART_IDS[@]}"; do
-        mkdir -p ~/CS744-assignment2/main/part$part_id/logs
-        cd ~/CS744-assignment2/main/part$part_id && ./run-p$part_id.sh 0 > ./logs/rank0 & # Run the master process
+        mkdir -p "${LOG_DIR}/part${part_id}"
+        cd ~/CS744-assignment2/main/part$part_id && ./run-p$part_id.sh 0 > "${LOG_DIR}/part${part_id}/rank0.log" 2>&1 & # Run the master process
         rank=1
         for worker_port in "${WORKERS_PORT[@]}"; do
-            sleep 5
+            sleep 2  # Ensure staggered starts to help avoid port conflicts
             part $part_id $rank $worker_port
             ((rank++))
         done
@@ -33,6 +35,8 @@ main() {
     done
 }
 
+# Ensure base log directory exists
+mkdir -p "$LOG_DIR"
 cd ~/CS744-assignment2; git pull
 parallel-ssh -i -h hostnames -O StrictHostKeyChecking=no "cd ~/CS744-assignment2; git pull"
 main
