46e642e9412e462415f26185c5303509e842a8c8
alicia-lyu <wlyu27@wisc.edu>
2024-02-21 11:22:23 -0600

    Printing error messages

--- run.sh
+++ run.sh
@@ -11,14 +11,14 @@ part() {
     local rank=$2
     local worker_port=$3
     # Redirect output to a log file within the log directory
-    ssh -p "$worker_port" "$HOST_NAME" "cd $HOME/CS744-assignment2/main/part$part_id && ./run-p$part_id.sh $rank" > "$LOG_DIR/part$part_id/rank$rank.log" 2>&1 &
+    ssh -p "$worker_port" "$HOST_NAME" "cd $HOME/CS744-assignment2/main/part$part_id && ./run-p$part_id.sh $rank" > "$LOG_DIR/part$part_id/rank$rank.log" &
     echo "Started part$part_id with rank $rank on $HOST_NAME:$worker_port"
 }
 
 part1() {
     mkdir -p "$LOG_DIR/part1"  # Ensure the directory exists
     echo "Starting part1"
-    cd $HOME/CS744-assignment2/main/part1 && ./run-p1.sh 0 > "$LOG_DIR/part1/log" 2>&1
+    cd $HOME/CS744-assignment2/main/part1 && ./run-p1.sh 0 > "$LOG_DIR/part1/log" 
 }
 
 main() {
@@ -26,7 +26,7 @@ main() {
     # part 2a, 2b, 3
     for part_id in "${PART_IDS[@]}"; do
         mkdir -p "$LOG_DIR/part$part_id" && ls "$LOG_DIR/part$part_id" || echo "Failed to create log directory for part$part_id"
-        cd $HOME/CS744-assignment2/main/part$part_id && ./run-p$part_id.sh 0 > "$LOG_DIR/part$part_id/rank0.log" 2>&1 & # Run the master process
+        cd $HOME/CS744-assignment2/main/part$part_id && ./run-p$part_id.sh 0 > "$LOG_DIR/part$part_id/rank0.log" & # Run the master process
         rank=1
         for worker_port in "${WORKERS_PORT[@]}"; do
             sleep 2  # Ensure staggered starts to help avoid port conflicts
