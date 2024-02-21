10102c94a5ff8d21714de9d4f634c6fb8b420268
alicia-lyu <wlyu27@wisc.edu>
2024-02-21 10:32:49 -0600

    Add logging

--- run.sh
+++ run.sh
@@ -8,20 +8,21 @@ part() {
     local part_id=$1
     local rank=$2
     local worker_port=$3
-    ssh -p "$worker_port" "$HOST_NAME" "cd ~/CS744-assignment2/main/part$part_id && ./run-p$part_id.sh $rank" &
+    ssh -p "$worker_port" "$HOST_NAME" "cd ~/CS744-assignment2/main/part$part_id && ./run-p$part_id.sh $rank" > "./logs/rank$rank" &
     echo "Started part$part_id with rank $rank on $HOST_NAME:$worker_port"
 }
 
 part1() {
     echo "Starting part1"
-    cd ~/CS744-assignment2/main/part1 && ./run-p1.sh 0
+    cd ~/CS744-assignment2/main/part1 && ./run-p1.sh 0 > ./part1.log
 }
 
 main() {
     part1
     # part 2a, 2b, 3
     for part_id in "${PART_IDS[@]}"; do
-        cd ~/CS744-assignment2/main/part$part_id && ./run-p$part_id.sh 0 & # Run the master process
+        mkdir -p ~/CS744-assignment2/main/part$part_id/logs
+        cd ~/CS744-assignment2/main/part$part_id && ./run-p$part_id.sh 0 > ./logs/rank0 & # Run the master process
         rank=1
         for worker_port in "${WORKERS_PORT[@]}"; do
             part $part_id $rank $worker_port
